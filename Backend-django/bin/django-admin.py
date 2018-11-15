86eb1d85b959bf7ac83e894a1e1706b9e7703809
Jeff Ma <zma67@wisc.edu>
2018-11-14 23:05:00 -0600

    Set up django backend environment

---
+++ Backend-django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jucjeff/Desktop/Projects/C4G/Coding4Good-Site/Backend-django/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
